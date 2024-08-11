317e43e7b257eb85b3c641c45497c0c48ada193b
MilesLittle <miles.little.dev@gmail.com>
2024-08-11 11:24:29 -0400

    Added seeded admin data

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
 
 
-    has_many :posts
+    has_many :posts, dependent: :destroy
     has_many :comments, dependent: :destroy
     has_one :inbox, dependent: :destroy
 
